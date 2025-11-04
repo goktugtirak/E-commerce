abbdda562f51e2cb8c4b5acd9d52aa74611e4b22
Goktug Tirak <goktugtrk9@gmail.com>
2025-11-04 13:42:50 +0300

    feat: List products on homepage from database

--- server/src/controllers/pageController.js
+++ server/src/controllers/pageController.js
@@ -1,6 +1,17 @@
-const getIndexPage = (req,res) => {
-    console.log('REQUEST USER:::', req.user);
-    res.render("index")
+import Product from '../models/productModel.js';
+
+const getIndexPage = async (req,res) => {
+    try{
+        const products = await Product.find();
+        res.render('index',{
+            products: products,
+            user: req.user
+        });
+    }catch(error){
+        console.error(error);
+        res.status(500).send('Sunucu Hatası');
+    }
+        console.log('REQUEST USER:::', req.user);
 }
 
 export {getIndexPage}
---
+++ server/src/routes/productRoute.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import {getProducts} from '../controllers/productController.js';
+
+const router = express.Router();
+
+
+export default router;
